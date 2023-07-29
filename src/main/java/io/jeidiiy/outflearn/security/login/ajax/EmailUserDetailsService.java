0304d1a5e15b0f8a7e1232d0a25320b04ceaa97f
JIYONG JUNG <jeidiiy98@gmail.com>
2023-07-29 15:41:47 +0900

    feat: Email로 User를 조회하는 DetailsService 구현

---
+++ src/main/java/io/jeidiiy/outflearn/security/login/ajax/EmailUserDetailsService.java
@@ -0,0 +1,27 @@
+package io.jeidiiy.outflearn.security.login.ajax;
+
+import java.util.Set;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import io.jeidiiy.outflearn.user.domain.User;
+import io.jeidiiy.outflearn.user.service.port.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class EmailUserDetailsService implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email).orElseThrow(() -> new EmailNotFoundException(email));
+		return LoginUser.of(user.getId(), user.getEmail(), user.getPassword(),
+			Set.of(new SimpleGrantedAuthority("ROLE_USER")));
+	}
+}
