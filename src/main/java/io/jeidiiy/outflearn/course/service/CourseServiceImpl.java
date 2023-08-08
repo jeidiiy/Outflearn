effb7e89670ed6e5764049f0cc16c44efb9e26ae
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-08 19:09:18 +0900

    feat: Course 서비스 계층 개발

---
+++ src/main/java/io/jeidiiy/outflearn/course/service/CourseServiceImpl.java
@@ -0,0 +1,28 @@
+package io.jeidiiy.outflearn.course.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.jeidiiy.outflearn.course.controller.port.CourseService;
+import io.jeidiiy.outflearn.course.domain.Course;
+import io.jeidiiy.outflearn.course.domain.CourseCreate;
+import io.jeidiiy.outflearn.course.service.port.CourseRepository;
+import io.jeidiiy.outflearn.user.domain.User;
+import io.jeidiiy.outflearn.user.service.port.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseService {
+
+	private final CourseRepository courseRepository;
+	private final UserRepository userRepository;
+
+	@Override
+	public Long create(CourseCreate courseCreate, Long creatorId) {
+		User creator = userRepository.getById(creatorId);
+		Course course = courseRepository.save(Course.from(courseCreate, creator));
+		return course.getId();
+	}
+}
