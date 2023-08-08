902bf4bc31188f11952ac132f083dd487d7bdf05
JIYONG JUNG <jeidiiy98@gmail.com>
2023-08-08 18:50:31 +0900

    feat: 회원가입 과정 간편화를 위한 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (id, email, nickname, password, status) VALUES (999999, 'wldyd2356@gmail.com', 'jeidiiy', '$2a$10$mLXRFrihpPDDc/iKr/Sqz.pcl6zqz45dyNCWhL8sCeZYo.jcZj1CW', 'ACTIVE');
