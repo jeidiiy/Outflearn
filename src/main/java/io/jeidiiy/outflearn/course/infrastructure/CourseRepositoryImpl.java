3bb42f5982b0d55d8e17fec47a8c6caa78ad8319
JIYONG JUNG <jeidiiy98@gmail.com>
2023-09-12 23:20:12 +0900

    feat: CourseRepository에 id 기반 조회 기능 추가 및 기존 코드 수정

--- src/main/java/io/jeidiiy/outflearn/course/infrastructure/CourseRepositoryImpl.java
+++ src/main/java/io/jeidiiy/outflearn/course/infrastructure/CourseRepositoryImpl.java
@@ -2,6 +2,7 @@ package io.jeidiiy.outflearn.course.infrastructure;
 
 import org.springframework.stereotype.Repository;
 
+import io.jeidiiy.outflearn.common.exception.ResourceNotFoundException;
 import io.jeidiiy.outflearn.course.domain.Course;
 import io.jeidiiy.outflearn.course.service.port.CourseRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,11 @@ import lombok.RequiredArgsConstructor;
 public class CourseRepositoryImpl implements CourseRepository {
 
 	private final CourseJpaRepository courseJpaRepository;
+
+	@Override
+	public Course findById(Long id) {
+		return courseJpaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Course", id));
+	}
 
 	@Override
 	public Course save(Course course) {
