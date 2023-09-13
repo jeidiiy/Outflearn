709c33aa29478b6f461c3b21a61723cf4a78b647
JIYONG JUNG <jeidiiy98@gmail.com>
2023-09-13 16:50:35 +0900

    feat: Section 생성 응답 DTO 개발

---
+++ src/main/java/io/jeidiiy/outflearn/section/controller/response/SectionCreateResponse.java
@@ -0,0 +1,12 @@
+package io.jeidiiy.outflearn.section.controller.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+public class SectionCreateResponse {
+	private Long id;
+}
