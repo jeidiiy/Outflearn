95f877d27134f5d009131c5551c64b4b003bc2f9
JIYONG JUNG <jeidiiy98@gmail.com>
2023-09-13 16:46:14 +0900

    feat: 리포지토리 인터페이스 작성

---
+++ src/main/java/io/jeidiiy/outflearn/section/service/port/SectionRepository.java
@@ -0,0 +1,7 @@
+package io.jeidiiy.outflearn.section.service.port;
+
+import io.jeidiiy.outflearn.section.domain.Section;
+
+public interface SectionRepository {
+	Section create(Section section);
+}
